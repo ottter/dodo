074323aeb3a4738446cac0dbb04134919319f697
ottter <jamescross808@gmail.com>
2019-12-19 17:57:00 -0500

    Adds Jebrim command

--- cogs/users.py
+++ cogs/users.py
@@ -1,12 +1,14 @@
 import discord
 import config
 import os
+import csv
 import random
 from discord.ext import commands
 
 imgdir = './images'
 
 def rename_folders(folder):
+    # Example: rename_folders(f'{imgdir}/lights/')
     i = 0
     for filename in os.listdir(folder):
         dst = str(i) + ".png"
@@ -15,11 +17,12 @@ def rename_folders(folder):
         os.rename(src, dst)
         i += 1
 
+
 class Users(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.cooldown(1, 3, commands.BucketType.user)
+    @commands.cooldown(1, 4, commands.BucketType.user)
     @commands.command()
     async def lights(self, context):
         # rename_folders(f'{imgdir}/lights/')
@@ -27,6 +30,14 @@ class Users(commands.Cog):
         file_count = len(files) - 1
         n = random.randint(0, file_count)
         await context.channel.send(file=discord.File(f'{imgdir}/lights/{n}.png'))
+
+    @commands.cooldown(1, 4, commands.BucketType.user)
+    @commands.command()
+    async def jebrim(self, context):
+        with open(f'{imgdir}/jebrim.csv') as f:
+            reader = csv.reader(f)
+            chosen_row = random.choice(list(reader))
+            await context.channel.send(chosen_row[0])
 
 
 def setup(bot):
