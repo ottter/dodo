457c48111204f8763687aa97d698c981ac25f78e
ottter <jamescross808@gmail.com>
2020-03-16 18:04:14 -0400

    move .coins to new API

--- config.py
+++ config.py
@@ -11,5 +11,7 @@ discord_game_played = 'Runescape'
 
 OWM_TOKEN = os.environ['OMW_API_KEY']
 
+COINMARKET_KEY = os.environ['CMC_API_KEY']
+
 MONGO_TOKEN = MongoClient(f"{os.environ['MONGO_PASSWORD']}")
 db = MONGO_TOKEN.get_database('discord')
