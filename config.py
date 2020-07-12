57e6a1b50dcd920b1bc9c852796c627465949127
Sajiel Chishti <sajielc@gmail.com>
2020-07-12 14:03:47 -0400

    Don't publicize passwords.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 server_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
-DISCORD_TOKEN = 'NDM2MzMxMzg0MjQwNDcyMDc1.Xm_4Xw.foUFkebL6a3Yleqs6ZLbCr5b-4s'
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
 discord_game_played = 'Runescape'
 
@@ -13,7 +13,7 @@ OWM_TOKEN = os.environ['OMW_API_KEY']
 
 COINMARKET_KEY = os.environ['CMC_API_KEY']
 
-MONGO_TOKEN = MongoClient("mongodb+srv://james:QZ6X1NJfhG6JIjcq@cluster0-wm7ma.mongodb.net/test?retryWrites=true&w=majority")
+MONGO_TOKEN = MongoClient(f"{os.environ['MONGO_PASSWORD']}")
 db = MONGO_TOKEN.get_database('discord')
 
 accepted_hosts = ['discord', 'imgur', 'youtube', 'gyazo']
