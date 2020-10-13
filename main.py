8b0833a043de2fd7c7b8ea13b6751808f1ab32e5
James <29265756+ottter@users.noreply.github.com>
2020-10-13 19:12:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,11 +73,6 @@ async def on_guild_remove(guild):
 
 @bot.event
 async def on_member_remove(member):
-    if member.guild.id == 563549980439347201:
-        channel = bot.get_channel(703752970894049320)
-        message = f'Cya {member} you dummy'
-        await channel.send(message)
-    # Cute coding ability, son
     if member.guild.id == 765694107690991647:
         channel = bot.get_channel(765695723244486716)
         message = f'Cya {member} you dummy'
